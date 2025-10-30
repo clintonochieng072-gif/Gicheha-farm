8a1937f0216876460d341357a5c936b7ed8f2589
Clinton Ochieng <clintonochieng072@gmail.com>
2025-10-30 18:17:39 +0300

    Fix CORS headers to allow cache-control headers for login/logout

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,13 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Cache-Control",
+    "Pragma",
+    "Expires",
+  ],
 };
 app.use(cors(corsOptions));
 
